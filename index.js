aa93250a2f623350ad13804d737ffb598a934943
Ike Mwithiga <mwithigaike@gmail.com>
2025-04-09 11:58:43 +0100

    index.js edits

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+
+function findMatching(array, majina) {
+    const namesWithAnS = array.filter(name => name.toLowerCase() === majina.toLowerCase())
+    return namesWithAnS
+}
+
+console.log(findMatching(drivers, "bobby"))
+
+
+function fuzzyMatch(array, string) {
+    return array.filter(name => name.toLowerCase().startsWith(string.toLowerCase()))
+}
+
+function matchName(array, name) {
+    const nameMatches = array.filter(user => user.name.toLowerCase() === name.toLowerCase())
+    return nameMatches
+}
